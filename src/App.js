d4037c7d63f955501f53ee5d1303474ec1e3b06f
Calwera <calwera@gmail.com>
2022-06-07 22:44:51 +0200

    7june end

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const expenses = [
     { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 12) },
-    {},
+    { title: "Pilot", amount: 200, date: new Date(2022, 3, 6) },
+    { title: "jonh", amount: 10, date: new Date(2020, 6, 3) },
+    { title: "Home", amount: 20, date: new Date(2022, 3, 6) },
   ];
 
   return (
@@ -13,22 +15,22 @@ function App() {
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>{" "}
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>{" "}
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>{" "}
+      />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
